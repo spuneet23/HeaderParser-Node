0b0be82587f6b9ad83d0386ca37ff3d5d664dfac
Puneet Singh <spuneet23@gmail.com>
2016-09-22 22:13:56 +0530

    Final commit

--- app.js
+++ app.js
@@ -2,14 +2,27 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var moment = require('moment');
+var locale = require('locale');
+var supported = ["en", "en_US", "ja"];
+var useragent = require('useragent');
 
+app.use(locale(supported));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 
 app.get('/',function(req,res){
   var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
   var lang = req.headers['accept-language'];
-  res.send("IP Address: "+ip+"      Lang: "+lang);
+  var user = req.headers['user-agent'];
+  var agent = useragent.parse(req.headers['user-agent']);
+  var headers= {
+  	"IP Address":ip,
+  	"Lang":req.locale,
+  	"Software":agent.os.family
+  };
+   var final=JSON.stringify(headers);
+   res.send(final);
+  // res.send("IP Address: "+ip+"      Lang: "+req.locale+"     User-agent:"+agent.os);
 });
 
 
